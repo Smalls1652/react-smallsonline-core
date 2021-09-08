437bd292dce60440490eef674a06992c54b65bd0
Timothy Small <smalls.online@icloud.com>
2021-09-07 23:30:13 -0400

    Updated file extensions
    - Non-JSX components utilize '.ts' for the file extensions instead of
      '.tsx'.
    - Fixed using 'Boolean' instead of 'boolean' in two interfaces as well.

--- src/elements/dropdown-menus/interfaces/ISimpleDropdownMenu.ts
+++ src/elements/dropdown-menus/interfaces/ISimpleDropdownMenu.ts
@@ -1,5 +1,5 @@
 export default interface ISimpleDropdownMenu {
     title: string;
-    isNavbarMenu: Boolean;
+    isNavbarMenu: boolean;
     children?: any;
 }
--- src/elements/list-groups/interfaces/ISimpleListGroup.ts
+++ src/elements/list-groups/interfaces/ISimpleListGroup.ts
@@ -1,5 +1,5 @@
 export default interface ISimpleListGroup {
-    flush: Boolean;
+    flush: boolean;
     className?: string;
     children?: any;
 }
