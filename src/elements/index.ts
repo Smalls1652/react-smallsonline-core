cc93888a76ecedbcbc499e442bdc3dcc884cba26
Timothy Small <smalls.online@icloud.com>
2021-09-07 23:20:31 -0400

    Moved main index.ts into elements
    - Created a new index.ts as well.

---
+++ src/elements/index.ts
@@ -0,0 +1,21 @@
+import { BootstrapRow } from './common';
+
+import { PageContentCard, ItemCard } from './cards';
+
+import { SimpleTable, SimpleTableItem } from './tables';
+
+import { SimpleListGroup, SimpleListGroupItem, SimpleListGroupItemHeader } from './list-groups';
+
+import { SimpleDropdownMenu } from './dropdown-menus';
+
+export {
+    BootstrapRow,
+    PageContentCard,
+    ItemCard,
+    SimpleTable,
+    SimpleTableItem,
+    SimpleListGroup,
+    SimpleListGroupItem,
+    SimpleListGroupItemHeader,
+    SimpleDropdownMenu
+}
